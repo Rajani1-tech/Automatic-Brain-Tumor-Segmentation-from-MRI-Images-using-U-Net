aae41e720cd20b6a193348f8a2b278eded780de7
Predator <rajanilami.12@gmail.com>
2026-03-18 19:49:31 +0545

    Change the size of the img

--- src/inference/predict.py
+++ src/inference/predict.py
@@ -9,10 +9,13 @@ class Predictor:
         self.model = UNetModel().model
         self.model.load_weights(model_path)
 
-    def predict(self, image_path, threshold=0.5):
+    def predict(self, image_path, threshold=0.5, return_resized=True):
         img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-        img = cv2.resize(img, (Config.IMG_WIDTH, Config.IMG_HEIGHT)) / 255.0
-        img = np.expand_dims(img, axis=(0,-1))
-        mask = self.model.predict(img)[0,...,0]
-        mask = (mask > threshold).astype(np.uint8) * 255
+        original_shape = img.shape
+        img_resized = cv2.resize(img, (Config.IMG_WIDTH, Config.IMG_HEIGHT)) / 255.0
+        img_resized = np.expand_dims(img_resized, axis=(0,-1))
+        mask = self.model.predict(img_resized)[0,...,0]
+        mask = (mask > threshold).astype(np.uint8)
+        if return_resized:
+            mask = cv2.resize(mask, (original_shape[1], original_shape[0]), interpolation=cv2.INTER_NEAREST)
         return mask
