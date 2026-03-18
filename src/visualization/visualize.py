56139ae3dc8aac1a40abe02bd42c6a40f659a7be
Predator <rajanilami.12@gmail.com>
2026-03-18 19:45:59 +0545

    Added visulaization for train & val loss

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,10 +1,8 @@
-# src/visualization/visualizer.py
 import matplotlib.pyplot as plt
 import cv2
-from src.inference.predict import Predictor
 
 class Visualizer:
-    def __init__(self, predictor: Predictor):
+    def __init__(self, predictor):
         self.predictor = predictor
 
     def show_prediction(self, image_path):
@@ -13,6 +11,7 @@ class Visualizer:
         img = cv2.resize(img, (mask.shape[1], mask.shape[0]))
 
         plt.figure(figsize=(10,5))
+
         plt.subplot(1,2,1)
         plt.title("Original Image")
         plt.imshow(img, cmap='gray')
@@ -20,4 +19,38 @@ class Visualizer:
         plt.subplot(1,2,2)
         plt.title("Predicted Mask")
         plt.imshow(mask, cmap='gray')
+
+        plt.show()
+
+    def plot_training_history(self, history, save_path="training_plot.png"):
+        hist = history.history
+
+        plt.figure(figsize=(12,8))
+
+        plt.subplot(2,2,1)
+        plt.plot(hist['loss'], label='Train Loss')
+        plt.plot(hist['val_loss'], label='Val Loss')
+        plt.title("Loss")
+        plt.legend()
+
+        plt.subplot(2,2,2)
+        plt.plot(hist['accuracy'], label='Train Acc')
+        plt.plot(hist['val_accuracy'], label='Val Acc')
+        plt.title("Accuracy")
+        plt.legend()
+
+        plt.subplot(2,2,3)
+        plt.plot(hist['dice_coef'], label='Train Dice')
+        plt.plot(hist['val_dice_coef'], label='Val Dice')
+        plt.title("Dice Score")
+        plt.legend()
+
+        plt.subplot(2,2,4)
+        plt.plot(hist['iou_coef'], label='Train IoU')
+        plt.plot(hist['val_iou_coef'], label='Val IoU')
+        plt.title("IoU Score")
+        plt.legend()
+
+        plt.tight_layout()
+        plt.savefig(save_path)
         plt.show()
