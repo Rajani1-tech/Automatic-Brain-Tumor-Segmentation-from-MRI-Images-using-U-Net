2060ab9a2d0efac5c7b83b3d2934a10b393bc38f
Predator <rajanilami.12@gmail.com>
2026-03-23 21:00:48 +0545

    Change the model saved format

--- configs/config.py
+++ configs/config.py
@@ -1,19 +1,16 @@
 # configs/config.py
 class Config:
-    
-    IMG_HEIGHT = 128
-    IMG_WIDTH = 128
+    IMG_HEIGHT = 240
+    IMG_WIDTH = 240
     IMG_CHANNELS = 1
-    NUM_CLASSES = 1  # Binary segmentation: tumor vs non-tumor
+    NUM_CLASSES = 1
 
-    
     BATCH_SIZE = 8
     EPOCHS = 50
     LEARNING_RATE = 1e-4
 
-    
     TRAIN_IMAGES_DIR = "data/train/images/"
     TRAIN_MASKS_DIR = "data/train/masks/"
     VAL_IMAGES_DIR = "data/val/images/"
     VAL_MASKS_DIR = "data/val/masks/"
-    MODEL_SAVE_PATH = "models_saved/unet_brain_tumor.h5"
+    MODEL_SAVE_PATH = "models_saved/unet_brain_tumor.pth"
