25745dce2835597147e6278cfeab374a1cfe49a7
Predator <rajanilami.12@gmail.com>
2026-03-23 21:03:31 +0545

    Added comparision of results of both models

--- src/evaluation/metrics.py
+++ src/evaluation/metrics.py
@@ -6,19 +6,20 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from configs.config import Config
 from src.inference.predict import Predictor
 
+
 class Evaluator:
     def __init__(self, predictor: Predictor):
         self.predictor = predictor
 
     def _dice_coefficient(self, y_true, y_pred):
-        smooth = 1.
+        smooth = 1.0
         y_true_f = y_true.flatten()
         y_pred_f = y_pred.flatten()
         intersection = np.sum(y_true_f * y_pred_f)
-        return (2. * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
+        return (2.0 * intersection + smooth) / (np.sum(y_true_f) + np.sum(y_pred_f) + smooth)
 
     def _iou_score(self, y_true, y_pred):
-        smooth = 1.
+        smooth = 1.0
         y_true_f = y_true.flatten()
         y_pred_f = y_pred.flatten()
         intersection = np.sum(y_true_f * y_pred_f)
@@ -34,17 +35,23 @@ class Evaluator:
 
         for img_name, mask_name in zip(images_list, masks_list):
             mask_gt = cv2.imread(os.path.join(masks_dir, mask_name), cv2.IMREAD_GRAYSCALE)
-            mask_gt = cv2.resize(mask_gt, (Config.IMG_WIDTH, Config.IMG_HEIGHT))
-            mask_gt = (mask_gt > 127).astype(np.int32)
+            if mask_gt is None:
+                continue
 
             mask_pred = self.predictor.predict(os.path.join(images_dir, img_name))
-            mask_pred = (mask_pred > 127).astype(np.int32)
 
-            y_true_all.extend(mask_gt.flatten())
-            y_pred_all.extend(mask_pred.flatten())
+            if mask_pred.shape != mask_gt.shape:
+                mask_pred = cv2.resize(mask_pred, (mask_gt.shape[1], mask_gt.shape[0]),
+                                       interpolation=cv2.INTER_NEAREST)
 
-            dice_scores.append(self._dice_coefficient(mask_gt, mask_pred))
-            iou_scores.append(self._iou_score(mask_gt, mask_pred))
+            mask_gt_bin = (mask_gt > 127).astype(np.int32)
+            mask_pred_bin = (mask_pred > 127).astype(np.int32)
+
+            y_true_all.extend(mask_gt_bin.flatten())
+            y_pred_all.extend(mask_pred_bin.flatten())
+
+            dice_scores.append(self._dice_coefficient(mask_gt_bin, mask_pred_bin))
+            iou_scores.append(self._iou_score(mask_gt_bin, mask_pred_bin))
 
         return {
             "accuracy": accuracy_score(y_true_all, y_pred_all),
